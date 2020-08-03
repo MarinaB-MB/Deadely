d3723288dc16bc127f31e4d3e24aa565459ad5e0
MarinaB <m.beloglazenko@ideas-world.com>
2020-08-03 10:33:40 +0300

    fix fragment_stat.xml

--- itlen/app/src/main/java/com/deadely/itl_en/ui/stat/presenter/StatPresenter.kt
+++ itlen/app/src/main/java/com/deadely/itl_en/ui/stat/presenter/StatPresenter.kt
@@ -1,11 +1,19 @@
 package com.deadely.itl_en.ui.stat.presenter
 
+import android.os.Bundle
 import com.deadely.itl_en.base.BasePresenter
+import com.deadely.itl_en.database.AppDatabase
+import com.deadely.itl_en.database.dao.UserDao
+import com.deadely.itl_en.network.IRestDBService
 import com.deadely.itl_en.ui.stat.IStatContract
 import javax.inject.Inject
 
-class StatPresenter : BasePresenter<IStatContract.View>, IStatContract.Presenter {
-    @Inject
-    constructor()
+class StatPresenter @Inject constructor(private var db: AppDatabase, private var apiInterface: IRestDBService) : BasePresenter<IStatContract.View>(), IStatContract.Presenter {
+    private lateinit var ud: UserDao
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ud = db.userDao()
+    }
 }
 
